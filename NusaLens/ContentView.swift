50da1cc1ad04e7ce237a0361f1381f5995687585
Ricky <ricreyracing@gmail.com>
2026-05-22 13:45:02 +0700

    Initial Commit

---
+++ NusaLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NusaLens
+//
+//  Created by student on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NusaLens/NusaLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NusaLensApp.swift
+//  NusaLens
+//
+//  Created by student on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NusaLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
