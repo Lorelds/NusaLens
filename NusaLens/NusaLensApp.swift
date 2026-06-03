0f92e9d3f16a1ee765c76a115e54ef56f5d45646
Amadeus Ian Gunadi <amadeusiangunadi19@gmail.com>
2026-06-03 16:06:28 +0700

    fix notifikasi

--- NusaLens/NusaLensApp.swift
+++ NusaLens/NusaLensApp.swift
@@ -7,9 +7,28 @@
 
 import SwiftUI
 import FirebaseCore
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    // This allows notifications to be shown even if the app is in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge, .list])
+    }
+}
 
 @main
 struct NusaLensApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init() {
         if Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil {
             FirebaseApp.configure()
